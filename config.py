e330fb22d66c9007924d9fc11c4a6cd74a03482c
Kousthubh  Bhat <93434418+Kousthubhbhat@users.noreply.github.com>
2023-08-19 21:08:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 import logging
 class Config:
     
-    API_ID = int(os.environ.get("API_ID", ""))
+    API_ID = int(os.environ.get("API_ID", "18274091"))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "Forward_BOT") 
