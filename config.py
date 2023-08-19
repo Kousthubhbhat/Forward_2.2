b7f4cdb9040d7954c856bd9d6755134cc087c19d
Kousthubh  Bhat <93434418+Kousthubhbhat@users.noreply.github.com>
2023-08-19 21:50:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     DATABASE_NAME = os.environ.get("DATABASE_NAME","Cluster0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Forward_data')
     SESSION = os.environ.get("SESSION", "Forward_Session")
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", ""))
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001796986025"))
     BOT_USERNAME= os.environ.get("BOT_USERNAME", None)
 
 
